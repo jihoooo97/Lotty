764195ee35e0c44b8e7cb8e4f2c265996af44639
Jiho <yjh7827@naver.com>
2022-04-17 22:43:31 +0900

    rx

---
+++ Lotty/UI/Base/QR/QrScanViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  QrScanViewModel.swift
+//  Lotty
+//
+//  Created by JihoMac on 2022/04/17.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class QrScanViewModel {
+
+    let disposeBag = DisposeBag()
+    
+    func setupBinding() {
+        
+    }
+    
+}
