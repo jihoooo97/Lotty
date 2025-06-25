156872901f4b90a69a09aa861431f0aa7b7d84b9
Jiho <yjh7827@naver.com>
2025-06-25 23:43:49 +0900

    chores: QR 리더 View dismiss button 추가

--- Projects/Features/SearchFeature/Sources/ViewController/QRViewController.swift
+++ Projects/Features/SearchFeature/Sources/ViewController/QRViewController.swift
@@ -11,8 +11,10 @@ import RxCocoa
 
 public final class QRViewController: BaseViewController {
 
+    private lazy var dismissButton = UIButton()
     private let qrReader = QRReaderView()
     private lazy var webView = WKWebView()
+    private lazy var descriptionImage = UIImageView()
     private lazy var descriptionLabel = UILabel()
     
     public override func viewDidLoad() {
@@ -29,6 +31,12 @@ public final class QRViewController: BaseViewController {
     
     
     private func bind() {
+        dismissButton.rx.tap
+            .asDriver()
+            .drive { [weak self] _ in
+                self?.dismiss(animated: true)
+            }.disposed(by: bag)
+        
         qrReader.qrUrl
             .distinctUntilChanged()
             .map { URLRequest(url: $0) }
@@ -38,6 +46,8 @@ public final class QRViewController: BaseViewController {
                 owner.webView.load(urlRequest)
                 
                 if !owner.descriptionLabel.isHidden {
+                    owner.descriptionImage.isHidden = true
+                    owner.descriptionImage.removeFromSuperview()
                     owner.descriptionLabel.isHidden = true
                     owner.descriptionLabel.removeFromSuperview()
                 }
@@ -45,6 +55,14 @@ public final class QRViewController: BaseViewController {
     }
     
     public override func setUIProperty() {
+        dismissButton = {
+            let button = UIButton()
+            button.configuration = .plain()
+            button.configuration?.image = .init(systemName: "xmark")
+            button.tintColor = .white
+            return button
+        }()
+        
         webView = {
             let webView = WKWebView()
             webView.scrollView.showsHorizontalScrollIndicator = false
@@ -52,10 +70,19 @@ public final class QRViewController: BaseViewController {
             return webView
         }()
         
+        descriptionImage = {
+            let imageView = UIImageView()
+            imageView.image = .icon(named: "icon_lomin")
+            imageView.tintColor = .lightGray.withAlphaComponent(0.5)
+            imageView.contentMode = .scaleAspectFit
+            return imageView
+        }()
+        
         descriptionLabel = {
             let label = UILabel()
             label.text = "QR코드를 촬영하여\n당첨정보를 조회하세요!"
             label.font = .systemFont(ofSize: 18.0, weight: .medium)
+            label.textColor = .lightGray.withAlphaComponent(0.5)
             label.textAlignment = .center
             label.numberOfLines = 0
             return label
@@ -63,8 +90,13 @@ public final class QRViewController: BaseViewController {
     }
     
     public override func setLayout() {
-        view.addSubviews(qrReader, webView)
-        webView.addSubview(descriptionLabel)
+        view.addSubviews(qrReader, webView, dismissButton)
+        webView.addSubviews(descriptionImage, descriptionLabel)
+        
+        dismissButton.snp.makeConstraints { make in
+            make.left.top.equalTo(safeArea).inset(20)
+            make.size.equalTo(30)
+        }
         
         qrReader.snp.makeConstraints { make in
             make.horizontalEdges.top.equalTo(safeArea)
@@ -76,8 +108,14 @@ public final class QRViewController: BaseViewController {
             make.top.equalTo(qrReader.snp.bottom)
         }
         
-        descriptionLabel.snp.makeConstraints { make in
+        descriptionImage.snp.makeConstraints { make in
             make.center.equalToSuperview()
+            make.size.equalTo(safeArea.snp.width).multipliedBy(0.5)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(descriptionImage)
         }
     }
 
