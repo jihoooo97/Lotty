5a11de83208c78b283a2a498e5f0ddb7312de327
Jiho <yjh7827@naver.com>
2025-06-29 01:17:23 +0900

    feat: 새로운 회차 알림 추가

--- Projects/App/Sources/AppDelegate.swift
+++ Projects/App/Sources/AppDelegate.swift
@@ -4,11 +4,18 @@ import GoogleMobileAds
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    let notificationCenter = UNUserNotificationCenter.current()
+    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         MobileAds.shared.start()
+        
+        notificationCenter.delegate = self
+        
+        notificationCenter.requestAuthorization(options: [.alert, .sound]) { _, _ in}
+        noticeDrawnDate()
         
         return true
     }
@@ -33,4 +40,73 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+}
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .list, .sound])
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        
+    }
+    
+    private func noticeDrawnDate() {
+        notificationCenter.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "\(checkRecentDrawNo())회 번호가 공개되었어요!"
+        content.subtitle = ""
+        content.body = "QR코드를 사용해서 당첨여부를 확인해보세요."
+        content.sound = .default
+        
+        let dateComponent = DateComponents(
+            calendar: .init(identifier: .gregorian),
+            timeZone: .init(identifier: "Asia/Seoul"),
+            hour: 20,
+            minute: 45,
+            weekday: 7
+        )
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: true)
+        
+        let request = UNNotificationRequest(
+            identifier: "DrawnDateNotice",
+            content: content,
+            trigger: trigger
+        )
+        
+        notificationCenter.add(request)
+    }
+    
+    private func checkRecentDrawNo() -> Int {
+        let calendar = Calendar(identifier: .gregorian)
+        let baseComponents = DateComponents(
+            timeZone: .init(identifier: "Asia/Seoul"),
+            year: 2002,
+            month: 12,
+            day: 7,
+            hour: 20,
+            minute: 45
+        )
+        
+        guard let baseDate = calendar.date(from: baseComponents) else { return 0 }
+        
+        let timeInterval = Date.now.timeIntervalSince(baseDate)
+        let weekSeconds: TimeInterval = 604800
+        let fullWeeksPassed = Int(timeInterval / weekSeconds)
+    
+        return 1 + fullWeeksPassed
+    }
+    
 }
